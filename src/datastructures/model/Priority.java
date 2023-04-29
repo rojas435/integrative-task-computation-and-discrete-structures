752ec89fc56a1fc9195155bb4c4e5bfff2550e0c
rojas435 <pradofeliperojas@gmail.com>
2023-04-29 15:53:35 -0500

    add controller class (Empty), add the priority class with the delete, insert, print methods

---
+++ src/datastructures/model/Priority.java
@@ -0,0 +1,74 @@
+package datastructures.model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class Priority <T extends  Comparable<T>> {
+    private ArrayList<T> heap;
+
+    public Priority(){
+        heap = new ArrayList<>();
+    }
+    private void swap(int index1, int index2) {
+        T temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+    public void print() {
+        System.out.println("Primera Clase\n");
+        for (T item : heap) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    public void insert(T item) {
+        heap.add(item);
+        int childIndex = heap.size() - 1;
+        int parentIndex = (childIndex - 1) / 2;
+
+        while (childIndex > 0 && heap.get(childIndex).compareTo(heap.get(parentIndex)) > 0) {
+            swap(childIndex, parentIndex);
+            childIndex = parentIndex;
+            parentIndex = (childIndex - 1) / 2;
+        }
+    }
+    public T delete() {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        T root = heap.get(0);
+        T lastItem = heap.remove(heap.size() - 1);
+        heap.set(0, lastItem);
+
+        int parent = 0;
+        int leftChild = 1;
+        int rightChild = 2;
+
+        while (leftChild < heap.size()) {
+            int maxChild = leftChild;
+            if (rightChild < heap.size() && heap.get(rightChild).compareTo(heap.get(leftChild)) > 0) {
+                maxChild = rightChild;
+            }
+
+            if (heap.get(parent).compareTo(heap.get(maxChild)) < 0) {
+                swap(parent, maxChild);
+                parent = maxChild;
+                leftChild = 2 * parent + 1;
+                rightChild = 2 * parent + 2;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+    public boolean isEmpty(){
+        return heap.isEmpty();
+    }
+}
--- src/datastructures/ui/Main.java
+++ src/datastructures/ui/Main.java
@@ -1,17 +1,82 @@
 package datastructures.ui;
-
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import datastructures.model.Controller;
 import java.util.Scanner;
 
 public class Main {
+    private Scanner reader;
+    private Controller controller;
+
+
+    public Main() {
+        reader = new Scanner(System.in);
+        controller = new Controller();
+    }
 
     public static void main(String[] args) {
+        Main main = new Main();
 
+        int option = -1;
+        do{
+            option = main.getOptionShowMenu();
+            main.executeOption(option);
+
+        }while(option != 0);
+    }
+
+    public int getOptionShowMenu(){
+        int option = 0;
+        printMenu();
+
+        option = validateIntegerOption();
+
+        return option;
+    }
+
+    public void printMenu(){
+        System.out.print(
+                "\n<<<<< Welcome to the boarding and disembarkation system >>>>>\n"+
+                        "1. Order of arrival\n"+
+                        "2. Embark\n"+
+                        "2. Disembark\n"+
+                        "0. Exit. \n"+
+                        "Option: ");
+    }
+
+    public void executeOption(int option){
+
+        switch(option){
+            case 1-> orderOfArrival();
+
+            case 2-> embark();
+
+            case 0-> System.out.println("Exit program.");
+
+            default-> System.out.println("Invalid Option");
+        }
+    }
+
+    /**
+     * @param: Option that gives the user
+     * @return: Validates the option and if the user gives a number that doesnt exist will give (Invalid Option) or even with letters (Invalid Option)
+     */
+    public int validateIntegerOption(){
+        int option = 0;
+
+        if(reader.hasNextInt()){
+            option = reader.nextInt();
+        }
+        else{
+            reader.nextLine();
+            option = -1;
+        }
+
+        return option;
+    }
+
+    public void embark(){
         
-
+    }
+    public void orderOfArrival(){
 
     }
 }
